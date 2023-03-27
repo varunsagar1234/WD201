b2f47052a9516240ccefee7058669cc0a35e34a4
varunsagar1234 <116007493+varunsagar1234@users.noreply.github.com>
2023-03-27 19:04:15 +0530

    Add files via upload

---
+++ http-server/index.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+let homeContent = "";
+let projectContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  rContent = registration;
+});
+
+const portNumber = require("minimist")(process.argv.slice(2));
+
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/registration":
+        response.write(rContent);
+        response.end();
+        break;
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(portNumber);
